b6989c240c4d45d2e7950d33252e626347aee8cb
Lindah001 <lindachiry2018@gmail.com>
2020-11-21 21:27:05 +0300

    hotel.css

---
+++ Desktop/project/form1.vb
@@ -0,0 +1,7 @@
+public class form1
+private sub Button1_click(Byval sender As system object, Byval e As a system.Eve
+Dim a,b,c As single
+a=TextBox1.TextBox
+b=TextBox2.TextBox
+c=(a^2+b^2)^(1/2)
+Label3.Text
